40504b84e852c14a50225b69c310bf34b7cae5fa
Bas Randsdorp <104019748+BMRandsdorp@users.noreply.github.com>
2023-03-01 13:32:39 +0100

    Delete src directory

--- src/data/dataConvert.js
+++
@@ -1,8 +0,0 @@
-let csvToJson = require("convert-csv-to-json");
-
-let fileInputName = "Studenten_Mock_Data.csv";
-let fileOutputName = "data.json";
-
-csvToJson.fieldDelimiter(",").getJsonFromCsv(fileInputName);
-csvToJson.formatValueByType().getJsonFromCsv(fileInputName);
-csvToJson.generateJsonFileFromCsv(fileInputName, fileOutputName);
