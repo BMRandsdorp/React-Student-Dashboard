52ffb5320c9385c6d1d6b17dd64ec923adb32686
Bas Randsdorp <104019748+BMRandsdorp@users.noreply.github.com>
2023-03-08 16:10:49 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import BarChart from "./components/barchart";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/pages/layout";
+import BarChart from "./components/pages/home";
+import Evelyn from "./components/pages/evelyn";
+import Aranka from "./components/pages/aranka";
+import Floris from "./components/pages/floris";
+import Hector from "./components/pages/hector";
+import Martina from "./components/pages/martina";
+import Maurits from "./components/pages/maurits";
+import Rahima from "./components/pages/rahima";
+import Sandra from "./components/pages/sandra";
+import Wietske from "./components/pages/wietske";
+import Storm from "./components/pages/storm";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Student assignment evaluations </h1>
-      <BarChart />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<BarChart />} />
+          <Route path="Evelyn" element={<Evelyn />} />
+          <Route path="Aranka" element={<Aranka />} />
+          <Route path="Floris" element={<Floris />} />
+          <Route path="Hector" element={<Hector />} />
+          <Route path="Martina" element={<Martina />} />
+          <Route path="Maurits" element={<Maurits />} />
+          <Route path="Rahima" element={<Rahima />} />
+          <Route path="Sandra" element={<Sandra />} />
+          <Route path="Wietske" element={<Wietske />} />
+          <Route path="Storm" element={<Storm />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/namelist.js
+++ src/components/namelist.js
@@ -3,13 +3,19 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { nameCheckChange } from "../actions";
 
-function NameList() {
+function NameListCheck() {
   const dispatch = useDispatch();
   // grab Names from state and turn array into ul>li>>checkbox
   const NameListData = useSelector((state) => state.nameList);
   const NamesListed = NameListData.map((name, key) => (
     <li key={key}>
-      <input type="checkbox" name={name} value={name} defaultChecked />
+      <input
+        type="checkbox"
+        name={name}
+        id={name}
+        value={name}
+        defaultChecked
+      />
       <label htmlFor={name}> {name} </label>
     </li>
   ));
@@ -21,18 +27,22 @@ function NameList() {
         'input[type="checkbox"]:checked'
       );
       checkedNames = Array.from(checked).map((check) => check.value);
-      // use checked names to filter through assignment data in reducer
+      // use checked names to filter through assignment data in reducer to change barchart data
       dispatch(nameCheckChange(checkedNames));
     }
   };
 
   return (
     <div className="filter-block">
-      <div> Check/uncheck to filter students evaluations </div>
+      <div>
+        Check/uncheck to filter students evaluations or go to the individual
+        student page in the navigation menu at
+        <a href="#top"> the top of page</a>
+      </div>
       <ul className="name-list" onChange={checkOnChange}>
         {NamesListed}
       </ul>
     </div>
   );
 }
-export default NameList;
+export default NameListCheck;
